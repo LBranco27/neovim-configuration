ffe8eca4b028ef6abbf915e50e37fdb758b4d2c7
Lucas Branco Alves de Melo <27lucasbranco27@gmail.com>
2026-04-21 14:15:05 -0300

    update

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -2,7 +2,11 @@ return {
 	{ "ellisonleao/gruvbox.nvim", priority = 1000 },
 	{ "nvim-tree/nvim-web-devicons" },
 	{ "preservim/nerdtree" },
-	{ "itchyny/lightline.vim" },
-	{ "shinchu/lightline-gruvbox.vim" },
-	{ "maximbaz/lightline-ale", dependencies = { "dense-analysis/ale", "itchyny/lightline.vim" } },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup()
+		end,
+	},
 }
